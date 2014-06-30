aa48d679f597cd7acf2e928bbc7d26d62b27ad2c
twlkyao <qishiyao2008@126.com>
2014-06-30 21:33:33 +0800

    Add more explaination.

--- src/com/twlkyao/variadic/Variadic.java
+++ src/com/twlkyao/variadic/Variadic.java
@@ -1,6 +1,13 @@
 package com.twlkyao.variadic;
 public class Variadic {
 	
+	/**
+	 * a method using variadic parameters.
+	 * @param s the first parameter.
+	 * @param ss the variadic parameter array,
+	 * a method can at most has one variadic parameter,
+	 * and it must be the last parameter.
+	 */
 	public static void m1(String s, String... ss) {
         for (int i = 0; i < ss.length; i++) {
             System.out.println(ss[i]);
